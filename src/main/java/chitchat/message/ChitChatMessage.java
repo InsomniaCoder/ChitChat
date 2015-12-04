b86eeca26533e666c9b4e197e5bd503ff17ae692
Tanat Lokejaroenlarb <opalcskmitl@gmail.com>
2015-12-05 03:48:30 +0700

    fill message component

--- src/main/java/chitchat/message/ChitChatMessage.java
+++ src/main/java/chitchat/message/ChitChatMessage.java
@@ -1,9 +1,17 @@
 package chitchat.message;
 
+import chitchat.Handler.ChitChatClient;
+
+import java.net.SocketAddress;
+import java.util.List;
+
 /**
  * Created by PorPaul on 5/12/2558.
  */
 public class ChitChatMessage {
 
-
+        MessageType messageType;
+        List<ChitChatClient> membersList;
+        String message;
+        SocketAddress socketAddress;
 }
