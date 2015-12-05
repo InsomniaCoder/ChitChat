917950e0dba8c157df4829aa7099b60073010ffd
Tanat Lokejaroenlarb <opalcskmitl@gmail.com>
2015-12-05 15:17:25 +0700

    refactor message to use Map accoardingly

--- src/main/java/chitchat/message/ChitChatMessage.java
+++ src/main/java/chitchat/message/ChitChatMessage.java
@@ -2,67 +2,67 @@ package chitchat.message;
 
 import java.net.Socket;
 import java.net.SocketAddress;
-import java.util.List;
+import java.util.Map;
 
 /**
  * Created by PorPaul on 5/12/2558.
  */
 public class ChitChatMessage {
 
-        MessageType messageType;
-        List<Socket> membersList;
-        String message;
-        SocketAddress socketAddress;
+    MessageType messageType;
+    Map<String, Socket> membersList;
+    String message;
+    SocketAddress socketAddress;
 
 
-        public ChitChatMessage(MessageType messageType, String message) {
-                this.messageType = messageType;
-                this.message = message;
-        }
+    public ChitChatMessage(MessageType messageType, String message) {
+        this.messageType = messageType;
+        this.message = message;
+    }
 
-        public ChitChatMessage(MessageType messageType, SocketAddress socketAddress) {
-                this.messageType = messageType;
-                this.socketAddress = socketAddress;
-        }
+    public ChitChatMessage(MessageType messageType, SocketAddress socketAddress) {
+        this.messageType = messageType;
+        this.socketAddress = socketAddress;
+    }
 
-        public ChitChatMessage(MessageType notify, List<java.net.Socket> membersList) {
-                this.messageType = notify;
-                this.membersList = membersList;
-        }
+    public ChitChatMessage(MessageType notify, Map<String, Socket> membersList) {
+        this.messageType = notify;
+        this.membersList = membersList;
+    }
 
-        public ChitChatMessage(MessageType messageType) {
-                this.messageType = messageType;
-        }
+    public ChitChatMessage(MessageType messageType) {
+        this.messageType = messageType;
+    }
 
-        public MessageType getMessageType() {
-                return messageType;
-        }
+    public MessageType getMessageType() {
+        return messageType;
+    }
 
-        public void setMessageType(MessageType messageType) {
-                this.messageType = messageType;
-        }
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
 
-        public List<Socket> getMembersList() {
-                return membersList;
-        }
+    public Map<String, Socket> getMembersList() {
+        return membersList;
+    }
 
-        public void setMembersList(List<Socket> membersList) {
-                this.membersList = membersList;
-        }
+    public void setMembersList(Map<String, Socket> membersList) {
+        this.membersList = membersList;
+    }
 
-        public String getMessage() {
-                return message;
-        }
+    public String getMessage() {
+        return message;
+    }
 
-        public void setMessage(String message) {
-                this.message = message;
-        }
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-        public SocketAddress getSocketAddress() {
-                return socketAddress;
-        }
+    public SocketAddress getSocketAddress() {
+        return socketAddress;
+    }
 
-        public void setSocketAddress(SocketAddress socketAddress) {
-                this.socketAddress = socketAddress;
-        }
+    public void setSocketAddress(SocketAddress socketAddress) {
+        this.socketAddress = socketAddress;
+    }
 }
