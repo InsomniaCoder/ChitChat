fc43116e9c8f4729a1a6baba611467cfcb39f12c
Tanat Lokejaroenlarb <opalcskmitl@gmail.com>
2015-12-05 13:24:53 +0700

    add more exception on ServerUpdater

--- src/main/java/chitchat/Handler/serverside/ServerUpdater.java
+++ src/main/java/chitchat/Handler/serverside/ServerUpdater.java
@@ -4,6 +4,8 @@ package chitchat.Handler.serverside;
  * Created by PorPaul on 5/12/2558.
  */
 
+import java.io.IOException;
+
 /**
  *
  * This class is used for being a checker to check the connection continuously.
@@ -21,6 +23,8 @@ public class ServerUpdater implements Runnable {
                 ServerHandler updater = ServerHandler.getInstance();
                 updater.checkMembersConnection();
             } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
