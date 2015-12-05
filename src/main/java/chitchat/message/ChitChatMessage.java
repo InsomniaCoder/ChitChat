d0e712a0c842bc6f5e959262c517965027c2fb87
Tanat Lokejaroenlarb <opalcskmitl@gmail.com>
2015-12-05 12:41:19 +0700

    add constructor and getter setter for Message

--- src/main/java/chitchat/message/ChitChatMessage.java
+++ src/main/java/chitchat/message/ChitChatMessage.java
@@ -1,7 +1,6 @@
 package chitchat.message;
 
-import chitchat.Handler.serverside.ChitChatClient;
-
+import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.List;
 
@@ -11,7 +10,59 @@ import java.util.List;
 public class ChitChatMessage {
 
         MessageType messageType;
-        List<ChitChatClient> membersList;
+        List<Socket> membersList;
         String message;
         SocketAddress socketAddress;
+
+
+        public ChitChatMessage(MessageType messageType, String message) {
+                this.messageType = messageType;
+                this.message = message;
+        }
+
+        public ChitChatMessage(MessageType messageType, SocketAddress socketAddress) {
+                this.messageType = messageType;
+                this.socketAddress = socketAddress;
+        }
+
+        public ChitChatMessage(MessageType notify, List<java.net.Socket> membersList) {
+                this.messageType = notify;
+                this.membersList = membersList;
+        }
+
+        public ChitChatMessage(MessageType messageType) {
+                this.messageType = messageType;
+        }
+
+        public MessageType getMessageType() {
+                return messageType;
+        }
+
+        public void setMessageType(MessageType messageType) {
+                this.messageType = messageType;
+        }
+
+        public List<Socket> getMembersList() {
+                return membersList;
+        }
+
+        public void setMembersList(List<Socket> membersList) {
+                this.membersList = membersList;
+        }
+
+        public String getMessage() {
+                return message;
+        }
+
+        public void setMessage(String message) {
+                this.message = message;
+        }
+
+        public SocketAddress getSocketAddress() {
+                return socketAddress;
+        }
+
+        public void setSocketAddress(SocketAddress socketAddress) {
+                this.socketAddress = socketAddress;
+        }
 }
