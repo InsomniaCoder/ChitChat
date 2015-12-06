07076ddf9a95b8499a44e1a5be47df6e8d90b909
Tanat Lokejaroenlarb <opalcskmitl@gmail.com>
2015-12-07 01:27:15 +0700

    change ChitChat Message to the new version and handle all constructor and getter setter

--- src/main/java/chitchat/message/ChitChatMessage.java
+++ src/main/java/chitchat/message/ChitChatMessage.java
@@ -1,9 +1,6 @@
 package chitchat.message;
 
-import java.net.InetAddress;
-import java.net.Socket;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by PorPaul on 5/12/2558.
@@ -11,42 +8,22 @@ import java.util.Map;
 public class ChitChatMessage {
 
     MessageType messageType;
-    Map<String, Socket> membersList;
-    List<String> members;
+    List<String> membersList;
     String message;
     String name;
-    InetAddress requestAddress;
-    int requestPort;
 
     public ChitChatMessage(MessageType messageType, String message) {
         this.messageType = messageType;
         this.message = message;
     }
 
-    public ChitChatMessage(MessageType notify, Map<String, Socket> membersList) {
+    public ChitChatMessage(MessageType notify, List<String> membersList) {
         this.messageType = notify;
         this.membersList = membersList;
     }
 
-
     public ChitChatMessage(MessageType messageType) {
         this.messageType = messageType;
-    }
-
-    public InetAddress getRequestAddress() {
-        return requestAddress;
-    }
-
-    public void setRequestAddress(InetAddress requestAddress) {
-        this.requestAddress = requestAddress;
-    }
-
-    public int getRequestPort() {
-        return requestPort;
-    }
-
-    public void setRequestPort(int requestPort) {
-        this.requestPort = requestPort;
     }
 
     public MessageType getMessageType() {
@@ -57,11 +34,11 @@ public class ChitChatMessage {
         this.messageType = messageType;
     }
 
-    public Map<String, Socket> getMembersList() {
+    public List<String> getMembersList() {
         return membersList;
     }
 
-    public void setMembersList(Map<String, Socket> membersList) {
+    public void setMembersList(List<String> membersList) {
         this.membersList = membersList;
     }
 
